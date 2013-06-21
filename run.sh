7d32c1f890f3cb72c6f50e2e5745e1a6965561d9
pchandra <pchandra@thoughtworks.com>
2013-06-21 14:15:26 +0530

    praveen|changes to run in CI

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-bundle exec rspec spec/features/initialize_data_spec.rb 
+bundle install
+bundle exec rspec spec/features/initialize_data_spec.rb
 bundle exec rspec spec/features/basic_sale_order_and_payment_spec.rb
 bundle exec rspec spec/features/cleanup_data_spec.rb 
--- spec/features/cleanup_data_spec.rb
+++ spec/features/cleanup_data_spec.rb
@@ -10,13 +10,13 @@ describe Ooor do
 
   describe "Cleanup Data" do
 
-    it "delete  product" do
+    it "should delete  product" do
       p_id=ProductProduct.search([['name', 'ilike', 'testProduct1']])[0]
       p = ProductProduct.find(p_id)
       p.destroy()
     end
 
-    it "should be able to create a customer" do
+    it "should delete customer" do
       p_id=ResPartner.search([['name', 'ilike', 'testCustomer1']])[0]
       p = ResPartner.find(p_id)
       p.destroy()
