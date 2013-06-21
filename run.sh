179239f08cc268afb5f0f59db4b6d0b590be67a1
pchandra <pchandra@thoughtworks.com>
2013-06-21 15:22:35 +0530

    praveen|return error on failure

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
+set -ex
 bundle install
-bundle exec rspec spec/features/initialize_data_spec.rb
-bundle exec rspec spec/features/basic_sale_order_and_payment_spec.rb
-bundle exec rspec spec/features/cleanup_data_spec.rb 
+bundle exec rspec spec/features/*.rb
